7d537d834a261dfc25d5df3c6a78078f92e9e648
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-01-16 17:54:32 +0200

    JS not finished

---
+++ src/main.js
@@ -0,0 +1,25 @@
+let menuBtn = document.querySelector('.open-menu');
+let menu = document.querySelector('.backdrop');
+let backdrop = document.querySelector('.backdrop')
+let menuItem = document.querySelectorAll('.menu-close');
+
+if (menuBtn) {
+  menuBtn.addEventListener('click', function () {
+    menu.classList.toggle('is-open');
+    backdrop.style.display = menu.classList.contains('is-open')
+      ? 'block'
+      : 'none'
+  });
+
+  menuItem.forEach(function (item) {
+    item.addEventListener('click', function () {
+      menu.classList.toggle('is-open');
+      backdrop.style.display = 'none';
+    });
+  });
+
+  backdrop.addEventListener('click', function () {
+    menu.classList.remove('is-open');
+    backdrop.style.display = 'none';
+  })
+}
