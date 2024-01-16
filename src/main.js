b7b5c9c77291140464355a67e2fc6e700fae55a4
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-01-17 00:13:08 +0200

    scale animation

--- src/main.js
+++ src/main.js
@@ -23,3 +23,24 @@ if (menuBtn) {
     backdrop.style.display = 'none';
   })
 }
+
+// scale for Nav Links
+// document
+//   .querySelectorAll('.nav-link, .footer-link')
+//   .forEach(function (navLink) {
+//     navLink.addEventListener('mouseenter', function () {
+//       var parentNavItem = navLink.closest('.nav-item, .footer-nav-list-item, .footer-socials-item');
+//       if (parentNavItem) {
+//         parentNavItem.style.transform = 'scale(1.1)';
+//       }
+//     });
+
+//     navLink.addEventListener('mouseleave', function () {
+//       var parentNavItem = navLink.closest(
+//         '.nav-item, .footer-nav-list-item, .footer-socials-item'
+//       );
+//       if (parentNavItem) {
+//         parentNavItem.style.transform = '';
+//       }
+//     });
+//   });
