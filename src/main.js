521cdcda8d2024a8cc9b64118bb68509e5a696cc
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-01-17 14:32:48 +0200

    last changes

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 let menuBtn = document.querySelector('.open-menu');
 let menu = document.querySelector('.mobile-menu');
-let backdrop = document.querySelector('.backdrop')
+let backdrop = document.querySelector('.backdrop');
 let menuItem = document.querySelectorAll('.menu-close');
 
 if (menuBtn) {
@@ -9,9 +9,11 @@ if (menuBtn) {
     backdrop.style.display = menu.classList.contains('is-open')
       ? 'block'
       : 'none';
-    document.body.style.overflow = menu.classList.contains('is-open') ? 'hidden' : 'auto';
+    document.body.style.overflow = menu.classList.contains('is-open')
+      ? 'hidden'
+      : 'auto';
   });
-  
+
   menuItem.forEach(function (item) {
     item.addEventListener('click', function () {
       menu.classList.toggle('is-open');
@@ -24,45 +26,28 @@ if (menuBtn) {
     menu.classList.remove('is-open');
     backdrop.style.display = 'none';
     document.body.style.overflow = 'auto';
-  })
+  });
 }
 
-// Removing mobile menu bg-img in landscape orientation
-  function updMobileMenuBg() {
-    var mobileMenu = document.querySelector('.mobile-menu');
-    var orientation = window.innerWidth > window.innerHeight ? 'landscape' : 'portrait';
-
-    if (orientation === 'landscape') {
-      mobileMenu.style.backgroundImage = 'none';
-    } else {
-      mobileMenu.style.backgroundImage = 'url(./img/menu/menu_mobile_1x.webp)';
-    }
-  }
-
-  updMobileMenuBg();
-
-  window.addEventListener('orientationchange', function () {
-    setTimeout(updMobileMenuBg, 1);
-  });
-
 // Scroll to top button
-let mybutton = document.getElementById("scroll-up-btn");
+let mybutton = document.getElementById('scroll-up-btn');
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 function scrollFunction() {
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-    mybutton.style.display = "block";
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    mybutton.style.display = 'block';
   } else {
-    mybutton.style.display = "none";
+    mybutton.style.display = 'none';
   }
 }
 
 mybutton.addEventListener('click', function () {
   window.scrollTo({
-    top: 0
-  })
-})
-
-
-
-
+    top: 0,
+  });
+});
